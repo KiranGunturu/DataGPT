c5d3dc8e9eb206057836080ea7a7102bd3b0541d
Kiran Gunturu <Kiran.gntdm@gmail.com>
2026-09-12 13:07:18 -0400

    memory history

---
+++ openai_memory_history.py
@@ -0,0 +1,19 @@
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+from mydb import run_query, get_schema
+
+load_dotenv()
+my_key = os.getenv('OPENAI_API_KEY')
+client = OpenAI(api_key=my_key)
+history = []
+while True:
+    user_input = input("Ask your question: ")
+    if user_input.lower() == 'exit':
+        break
+    history.append({"role": "user", "content": user_input})
+    print(history)
+    response = client.responses.create(model='gpt-5.6-sol',
+                                        input = history)
+    history.append({"role": "assistant", "content": response.output_text})
+    print(response.output_text)
